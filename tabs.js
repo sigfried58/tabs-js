977b6e4299dc4f3cb033f1566ab47096f0bed640
jimmy ibanez <jimmy.ibanez@outlook.com>
2020-04-10 22:25:30 -0400

    Initial commit

---
+++ tabs.js
@@ -0,0 +1,27 @@
+const tabs = document.querySelector('.tabs');
+const tabButtons = tabs.querySelectorAll('[role="tab"');
+const tabPanels = Array.from(tabs.querySelectorAll('[role="tabpanel"'));
+
+function handleTabClick(event) {
+  // Hide all tab panels
+  tabPanels.forEach((panel) => (panel.hidden = true));
+  // Mark all tabs as unselected
+  tabButtons.forEach((tab) => tab.setAttribute('aria-selected', false));
+  // Mark the clicked tab as selected
+  event.currentTarget.setAttribute('aria-selected', true);
+  // Find the associated tabPanel and show it!
+  const { id } = event.currentTarget;
+  // METHOD 1
+  // const tabPanel = tabs.querySelector(`[aria-labelledby="${id}"]`);
+  // tabPanel.hidden = false;
+
+  // METHOD 2 - Find in the array of tabPanels
+  const tabPanel = tabPanels.find(
+    (panel) => panel.getAttribute('aria-labelledby') === id
+  );
+  tabPanel.hidden = false;
+}
+
+tabButtons.forEach((button) =>
+  button.addEventListener('click', handleTabClick)
+);
